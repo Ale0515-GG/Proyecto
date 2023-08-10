53ccd0583b15196e04bfbb4e0a8326f12730380e
RamiroHerreraX <116208892+RamiroHerreraX@users.noreply.github.com>
2023-08-10 09:52:09 -0600

    RH

--- server/build/keys.js
+++ server/build/keys.js
@@ -4,7 +4,7 @@ exports.default = {
     database: {
         host: 'localhost',
         user: 'root',
-        password: '123456',
+        password: '1234',
         database: 'SaludParaTi'
     }
 };
