7633546eefa0a23ab9c0197fc780278512ae1fa3
Ale0515-GG <116208731+Ale0515-GG@users.noreply.github.com>
2023-08-11 09:45:16 -0600

    mk

--- client/src/app/componentes/medicos/medicos.component.ts
+++ client/src/app/componentes/medicos/medicos.component.ts
@@ -1,5 +1,7 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Medi } from 'src/app/models/Medico';
+import { MedicoService } from 'src/app/service/medico.service';
 
 @Component({
   selector: 'app-medicos',
@@ -21,13 +23,15 @@ export class MedicosComponent implements OnInit{
 ngOnInit(): void {
 }
 
-saveNewGame(){ //generamos el metodo
+constructor(private mediService: MedicoService,private router:Router){}
+
+saveNewMedi(){ //generamos el metodo
   // console.log(this.game);
 
-  this.gameService.saveGame(this.game).subscribe(
+  this.mediService.saveMedi(this.medi).subscribe(
     res =>{
       console.log(res);
-      this.router.navigate(['/games']);
+      this.router.navigate(['/medicos']);
     },
     err => console.error(err)
   )
--- client/src/app/service/medico.service.ts
+++ client/src/app/service/medico.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Medi } from '../models/Medico';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MedicoService {
 
-  constructor() { }
+  API_URI='http://localhost:3000/api'
+
+  constructor(private http:HttpClient) { }
+  getMedicos(){
+      return this.http.get(`${this.API_URI}/medicos`);
+  }
+
+  getMedi(id:string){
+    return this.http.get(`${this.API_URI}/medicos/${id}`);
+  }
+
+  deleteMedi(id:string){
+    return this.http.delete(`${this.API_URI}/medicos/${id}`);
+  }
+
+  saveMedi(medi:Medi){
+    return this.http.post(`${this.API_URI}/medicos`,medi);
+  }
+
+  updateMedi(id:string, updateMedi:Medi ): Observable<Medi>{
+    return this.http.put(`${this.API_URI}/medicos/${id}`,updateMedi);
+  }
+
 }
