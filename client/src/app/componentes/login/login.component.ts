afdc6b5804d85e9e9fde43f941145a83ca5f8c20
Luisgatovolador <116209151+Luisgatovolador@users.noreply.github.com>
2023-08-19 16:28:24 -0600

    No :3

--- client/src/app/componentes/login/login.component.ts
+++ client/src/app/componentes/login/login.component.ts
@@ -1,35 +1,17 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+// login.component.ts
+import { Component } from '@angular/core';
 import { LoginService } from 'src/app/service/Login/login.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Login } from '../../models/Login';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-  @HostBinding('class') clases = 'row';
-  login: any[] = [];
-
+export class LoginComponent {
   Correo = '';
-  Contrasena = '';
+  Contra = '';
 
-  constructor( private loginService: LoginService,private router: Router,private activeRoute: ActivatedRoute) {}
-
-  ngOnInit() {
-    this.getLogin();
-  }
-
-  getLogin() {
-    const params =this.activeRoute.snapshot.params;
-    this.loginService.getLogin(params['id']
-    ).subscribe(
-      res => {
-        this.login = res;
-      },
-      err => console.log(err)
-    );
-  }
+  constructor(private loginService: LoginService, private router: Router) {}
 
 }
