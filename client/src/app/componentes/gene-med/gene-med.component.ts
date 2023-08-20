a8ca6fc417996c4ec111910e2dc7c689c6e91496
RoxGDS0532 <141853929+RoxGDS0532@users.noreply.github.com>
2023-08-20 17:06:44 -0600

    Revert "Revert "Merge branch 'main' of https://github.com/Ale0515-GG/Proyecto""
    This reverts commit 9de54fd09906d8760ecfc2ee915818ac900e1289.

--- client/src/app/componentes/gene-med/gene-med.component.ts
+++ client/src/app/componentes/gene-med/gene-med.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Medico } from 'src/app/models/Medico';
 import { MedicoService } from 'src/app/service/medico.service';
@@ -8,34 +8,26 @@ import { MedicoService } from 'src/app/service/medico.service';
   templateUrl: './gene-med.component.html',
   styleUrls: ['./gene-med.component.css']
 })
-export class GeneMEDComponent {
-  @HostBinding('class') clases ='row';
-
+export class GeneMEDComponent implements OnInit {
   medi: Medico = {
-    Id:0,
-    Nombre:'',
-    Especialidad:'',
-    Telefono:'',
-   Correo:''
-};
+    Id: 0,
+    Nombre: '',
+    Especialidad: '',
+    Telefono: '',
+    Correo: ''
+  };
 
-ngOnInit(): void {
+  constructor(private mediService: MedicoService, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  saveNewMedi() {
+    this.mediService.saveMedi(this.medi).subscribe(
+      res => {
+        console.log(res);
+        this.router.navigate(['/medicos']);
+      },
+      err => console.error(err)
+    );
+  }
 }
-
-constructor(private mediService: MedicoService,private router:Router){}
-
-saveNewMedi(){ //generamos el metodo
-  // console.log(this.game);
-  delete this.medi.Id;
-
-  this.mediService.saveMedi(this.medi).subscribe(
-    res =>{
-      console.log(res);
-      this.router.navigate(['/medicos']);
-    },
-    err => console.error(err)
-  )
-}
-
-}
-
--- client/src/app/componentes/medicos/medicos.component.ts
+++ client/src/app/componentes/medicos/medicos.component.ts
@@ -1,4 +1,4 @@
-import { Component,HostBinding,OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { MedicoService } from 'src/app/service/medico.service'; 
 
 @Component({
@@ -6,36 +6,34 @@ import { MedicoService } from 'src/app/service/medico.service';
   templateUrl: './medicos.component.html',
   styleUrls: ['./medicos.component.css']
 })
-
-export class GameListComponent implements	 OnInit{
+export class MedicosComponent implements OnInit {
   @HostBinding('class') classes='row';
-  games:any =[]
-  constructor(private gameService:MedicoService){
+  medicos: any = [];
 
-  }
-  ngOnInit(){
+  constructor(private medicoService: MedicoService) {}
+
+  ngOnInit() {
     this.getMedicos();
   }
 
   getMedicos(){
-    this.gameService.getMedicos().subscribe(
-      // res => console.log(res),
-      res => {
-        this.games=res;
+    this.medicoService.getMedicos().subscribe(
+      res=> {
+        this.medicos = res;
       },
       err => console.log(err)
-    )
+      )
   }
 
-  deleteMedi(id: string){
-    // console.log(id);//se lo manda a consola
-    this.gameService. deleteMedi(id).subscribe(
-      res =>{
-        console.log(res)//mueste lo de la api, aqui se puede poner lo de registro eliminado
-        this.getMedicos();
-      },
-      err => console.log(err)
-    )
+  deleteMedico(id: number | undefined) {
+    if (id !== undefined) {
+      this.medicoService.deleteMedi(id.toString()).subscribe(
+        res => {
+          console.log(res);
+          this.getMedicos();
+        },
+        (err: any) => console.error('Error al eliminar médico:', err)
+      );
+    }
   }
-
 }
