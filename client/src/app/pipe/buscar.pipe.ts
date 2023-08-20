6c359f858ed8b6d3f75cdf58db5430bc590c1c5b
RoxGDS0532 <141853929+RoxGDS0532@users.noreply.github.com>
2023-08-20 16:01:56 -0600

    noti paciente

--- client/src/app/pipe/buscar.pipe.ts
+++ client/src/app/pipe/buscar.pipe.ts
@@ -1,17 +1,25 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
+import { ToastrService } from 'ngx-toastr';
 @Pipe({
   name: 'buscar'
 })
 export class BuscarPipe implements PipeTransform {
 
-transform(value: any, arg: any):any {
-  const resultPost =[];
-  for(const post of value){
-    if(post.Nombre.toLowerCase().indexOf(arg.toLowerCase())>-1){
-      resultPost.push(post);
-    };
-  };
-  return resultPost;
-}
+  constructor(private toastrService: ToastrService) {} // Inyecta el servicio de notificaciones
+
+  transform(value: any, arg: any): any {
+    const resultPost = [];
+    for (const post of value) {
+      if (post.Nombre.indexOf(arg) > -1) {
+        resultPost.push(post);
+      }
+    }
+  
+    if (resultPost.length === 0) {
+      this.toastrService.warning('No se encontraron resultados', 'Aviso');
+    }
+  
+    return resultPost;
+  }
+  
 }
