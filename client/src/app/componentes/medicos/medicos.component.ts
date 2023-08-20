2b4e94a38b4ace5a248dc8b02b96c664dcb4ab3e
Ale0515-GG <116208731+Ale0515-GG@users.noreply.github.com>
2023-08-19 18:04:38 -0600

    d

--- client/src/app/componentes/medicos/medicos.component.ts
+++ client/src/app/componentes/medicos/medicos.component.ts
@@ -25,78 +25,13 @@ export class MedicosComponent implements OnInit {
     // Initialization code if needed
   }
 
-
-// <<<<<<< HEAD
-//   saveNewMedi() {
-//     this.mediService.saveMedi(this.medi).subscribe(
-//       res => {
-//         console.log(res);
-//         this.router.navigate(['/medicos']);
-//       },
-//       err => console.error(err)
-//     );
-//   }
-// }
-
-// @Component({
-//   selector: 'app-gene-med',
-//   template: `
-//     <!-- Your template for GeneMEDComponent -->
-//   `,
-//   // styleUrls: ... if needed
-// })
-// export class GeneMEDComponent implements OnInit {
-//   @HostBinding('class') classes = 'row';
-//   medi: any = [];
-
-//   constructor(private medicoService: MedicoService) {}
-
-//   ngOnInit() {
-//     this.getMedicos();
-//   }
-
-//   getMedicos() {
-//     this.medicoService.getMedicos().subscribe(
-//       res => {
-//         this.medi = res;
-//       },
-//       err => console.log(err)
-//     );
-//   }
-
-//   deleteMedi(id: string) {
-//     this.medicoService.deleteMedi(id).subscribe(
-//       res => {
-//         console.log(res);
-//         this.getMedicos();
-//       },
-//       err => console.log(err)
-//     );
-//   }
-// // saveNewMedi(){ //generamos el metodo
-// //   // console.log(this.game);
-
-// //   this.mediService.saveMedi(this.medi).subscribe(
-// //     res =>{
-// //       console.log(res);
-// //       this.router.navigate(['/medicos']);
-// //     },
-// // //     err => console.error(err)
-// // //   )
-// // // }
-
-// // // }
-
-// =======
-//   this.mediService.saveMedi(this.medi).subscribe(
-//     res =>{
-//       console.log(res);
-//       this.router.navigate(['/medicos']);
-//     },
-//     err => console.error(err)
-//   )
-// }
-
-// }
-
-// >>>>>>> 7f0efffafc490f7f936df46c5c645e62fc766335
+   saveNewMedi() {
+     this.mediService.saveMedi(this.medi).subscribe(
+       res => {
+         console.log(res);
+         this.router.navigate(['/medicos']);
+       },
+       err => console.error(err)
+     );
+   }
+ }
