988c922a17ad60f56e610eb9c94a9cad913d254a
RoxGDS0532 <141853929+RoxGDS0532@users.noreply.github.com>
2023-08-20 17:24:59 -0600

    masomenos

--- client/src/app/pipe/buscar.pipe.ts
+++ client/src/app/pipe/buscar.pipe.ts
@@ -4,14 +4,13 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'buscar'
 })
 export class BuscarPipe implements PipeTransform {
-
-transform(value: any, arg: any):any {
-  const resultPost =[];
-  for(const post of value){
-    if(post.Nombre.toLowerCase().indexOf(arg.toLowerCase())>-1){
-      resultPost.push(post);
+  transform(value: any, arg: any):any {
+    const resultPost =[];
+    for(const post of value){
+      if(post.Nombre.toLowerCase().indexOf(arg.toLowerCase())>-1){
+        resultPost.push(post);
+      };
     };
-  };
-  return resultPost;
-}
-}
+    return resultPost;
+  }
+  }
