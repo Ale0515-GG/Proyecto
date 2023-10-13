22bddfb37c4916bd763a7b4be94134aec7b319a1
RamiroHerreraX <116208892+RamiroHerreraX@users.noreply.github.com>
2023-10-13 11:39:51 -0600

    c

--- client/src/app/componentes/principal/principal.component.ts
+++ client/src/app/componentes/principal/principal.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
+*/
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment'; // Importa environment
+import * as mapboxgl from 'mapbox-gl';
+/*
+
+
+export class PrincipalComponent {
+
+}
+*/
 
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.component.html',
   styleUrls: ['./principal.component.css']
 })
-export class PrincipalComponent {
+/*export class AppComponent {
+  title = 'App-Articulos';
+}*/
 
+export class PrincipalComponent implements OnInit {
+  // Se crea el titulo
+  title = 'App-Articulos';
+
+  // Se crea definición de diseño y propiedades
+  map:mapboxgl.Map | undefined;
+  style = 'mapbox://stylee/mapbox/streets-v11'
+  lat = 21.15754419309309;
+  lng = -100.93253874733172;
+  zoom = 5;
+
+  constructor() {
+    (mapboxgl as any).accessToken = environment.mapbox.mapboxAccessToken;
+  }
+
+  ngOnInit(): void {
+    (this.buildMap());
+  }
+
+  buildMap(){
+
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: this.style,
+      zoom: this.zoom,
+      center: [this.lng, this.lat],
+      accessToken: environment.mapbox.mapboxAccessToken,
+      attributionControl: false,
+
+    });
+
+    const navControl = new mapboxgl.NavigationControl({
+      visualizePitch: true
+    })
+
+    this.map.addControl(navControl, 'top-right');
+    this.map.addControl(new mapboxgl.GeolocateControl({
+      positionOptions:{
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true,
+      showUserHeading: true
+    }));
+
+
+  }
 }
+
---
+++ client/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    mapbox:{
+        mapboxAccessToken: 'pk.eyJ1IjoiMTctcm94YW5hIiwiYSI6ImNsbjNhNmltMTA3cmEybXJ3Yjl6cmgzanIifQ.jYy2crPTScNlFvTJpGsh1g'
+    }
+    
+}
