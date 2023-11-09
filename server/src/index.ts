fd49f53c5ff34bb41afde52c1bd4907eea871bf3
RoxGDS0532 <rox.renteria1234@gmail.com>
2023-11-08 21:04:11 -0600

    verificar si funciona

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,60 +1,76 @@
-//Servidor
-import express,{Application, Router} from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-//Pagina
-import indexRoutes from './routes/indexRoutes';
-import pacienteRoutes from './routes/pacienteRoutes';
-import medicoRoutes from './routes/medicoRoutes';
-import citaRoutes from './routes/citaRoutes';
-import expedienteRoutes from './routes/expedienteRoutes';
-import loginRoutes from './routes/loginRoutes';
-
-//streaming
+import express, { Express, Request, Response } from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { createServer } from "http";
+//import { Server as SocketIOServer, Socket } from "socket.io"; // Importa Socket para definir el tipo de 'socket'
 
 
+import indexRoutes from "./routes/indexRoutes";
+import pacienteRoutes from "./routes/pacienteRoutes";
+import medicoRoutes from "./routes/medicoRoutes";
+import citaRoutes from "./routes/citaRoutes";
+import expedienteRoutes from "./routes/expedienteRoutes";
+import loginRoutes from "./routes/loginRoutes";
 
-// console.log("Serve");
+interface UserData {
+    roomName: string;
+    // Agrega otros campos si es necesario
+  }
+class Server {
+  private app: Express;
+  private server: any;
+  //private io: SocketIOServer;
 
-class Server{
-    public app: Application;
+  constructor() {
+    this.app = express();
+    this.server = createServer(this.app);
+    //this.io = new SocketIOServer(this.server);
+    this.config();
+    this.routes();
+    //this.sockets();
+  }
 
-    constructor(){ 
-        this.app = express();
-        this.config();//configura app
-        this.routes();//rutas
-    }
+  private config(): void {
+    this.app.set("port", process.env.PORT || 3000);
+    this.app.use(morgan("dev"));
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+  }
 
-    config():void{
-        this.app.set('port',process.env.PORT || 3000);//puerto 3000
-        this.app.use(morgan('dev'));//ver las peticiones cuando alguien entre a una ruta
-        this.app.use(cors());//pedir datos
-        this.app.use(express.json());//entender los fomratos json
-        this.app.use(express.urlencoded({extended: false}))//enviar datos desde un fromulario html
-    }
+  private routes(): void {
+    this.app.use("/", indexRoutes);
+    this.app.use("/api/paciente", pacienteRoutes);
+    this.app.use("/api/medico", medicoRoutes);
+    this.app.use("/api/cita", citaRoutes);
+    this.app.use("/api/expediente", expedienteRoutes);
+    this.app.use("/api/login", loginRoutes);
+    // this.app.use('/api/mapa',mapaRoutes);
+  }
 
-    routes():void{
-        this.app.use('/',indexRoutes);
-        this.app.use('/api/paciente',pacienteRoutes);
-        this.app.use('/api/medico',medicoRoutes);
-        this.app.use('/api/cita',citaRoutes);
-        this.app.use('/api/expediente',expedienteRoutes);
-        this.app.use('/api/login',loginRoutes);
-        //this.app.use('/api/mapa',mapaRoutes);
+  /*private sockets(): void {
+    this.io.on("connection", (socket: Socket) => {
+      console.log("Un usuario se ha conectado");
 
+      socket.on("join", (data: UserData) => {
+        const roomName = data.roomName;
+        socket.join(roomName);
+        socket.to(roomName).broadcast.emit("new-user", data);
+        console.log("Usuario conectado", data);
 
-
-    }
-
-    //para iniciar el servidor
-    start():void{
-        this.app.listen(this.app.get('port'),()=>{
-            console.log('Server on port',this.app.get('port'));//si se conecta nos manda el mensaje
+        socket.on("disconnect", () => {
+          socket.to(roomName).broadcast.emit("bye-user", data);
         });
-    }
+      });
+    });
+  }*/
+
+  public start(): void {
+    this.server.listen(this.app.get("port"), () => {
+      console.log("Server on port", this.app.get("port"));
+    });
+  }
 }
 
 const server = new Server();
 server.start();
-
-
