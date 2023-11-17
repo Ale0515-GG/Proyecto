35076aaf72f59091af691f184476b0065384f3d7
RoxGDS0532 <rox.renteria1234@gmail.com>
2023-11-17 11:24:27 -0600

    carrito completo

--- client/src/app/componentes/cart/cart.component.ts
+++ client/src/app/componentes/cart/cart.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CartItemModel } from 'src/app/models/cart-item-model';
 import { Product } from 'src/app/models/product';
 import { MessageService } from 'src/app/service/message.service';
+import { StorageService } from '../../service/storage.service';
 
 @Component({
   selector: 'app-cart',
@@ -11,13 +12,19 @@ import { MessageService } from 'src/app/service/message.service';
 export class CartComponent implements OnInit{
 
   cartItems: CartItemModel[] = [];
+  total=0;
 
-  constructor(private messageService: MessageService){
+  constructor(private messageService: MessageService,
+    private storageService:StorageService){
 
   }
 
   ngOnInit(): void {
+    if(this.storageService.existsCart()) {
+      this.cartItems = this.storageService.getCart();
+    }
     this.getItem();
+    this.total = this.getTotal();
   }
 
   getItem():void{
@@ -33,6 +40,32 @@ export class CartComponent implements OnInit{
         const cartItem = new CartItemModel(product);
         this.cartItems.push(cartItem);
       }
+      this.total = this.getTotal();
+      this.storageService.setCart(this.cartItems);
     });
+  }
+
+  getTotal(): number {
+    let total = 0;
+    this.cartItems.forEach(item =>{
+      total += item.qty * item.productPrice;
+    });
+    return + total.toFixed(2);
+  }
+
+  vaciarCart(): void{
+    this.cartItems =[];
+    this.total=0;
+    this.storageService.clear();
+  }
+
+  deleteItem(i:number):void{
+    if(this.cartItems[i].qty >1){
+      this.cartItems[i].qty--;
+    }else{
+      this.cartItems.splice(i,1);
+    }
+    this.total = this.getTotal();
+    this.storageService.setCart(this.cartItems);
   }
 }
