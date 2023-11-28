6dca4682fdd0ef4af79e75906c58c6c1239cda54
RoxGDS0532 <rox.renteria1234@gmail.com>
2023-11-27 23:30:53 -0600

    API paypal completa

--- client/src/app/componentes/farmacia/farmacia.component.ts
+++ client/src/app/componentes/farmacia/farmacia.component.ts
@@ -6,42 +6,6 @@ declare var paypal:any;
   templateUrl: './farmacia.component.html',
   styleUrls: ['./farmacia.component.css']
 })
-export class FarmaciaComponent implements OnInit{
-  @ViewChild('paypal', {static:true}) paypalElement! : ElementRef;
+export class FarmaciaComponent{
 
-  producto = {
-    descripcion : 'producto en venta',
-    precio      : 599.99,
-    img         : 'imagen de tu producto'
-
-  }
-
-
-  title = 'App-Articulos';
-    ngOnInit(): void {
-     paypal
-     .Buttons({
-      createOrder: (data:any,actions:any) => {
-        return actions.order.create({
-          purchase_units:[
-            {
-              description: this.producto.descripcion,
-              amount     :{
-                currency_code: 'MXN',
-                value        : this.producto.precio
-              }
-            }
-          ]
-        })
-      },
-      onApprove: async(data:any,actions:any) =>{
-        const order = await actions.order.capture();
-        console.log(order);
-      },
-      onError: (err: any) => {
-        console.log(err);
-      }
-     })
-     .render (this.paypalElement.nativeElement);
-    }
 }
